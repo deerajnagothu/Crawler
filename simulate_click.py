3069c52a2621ff93d8a4002d57c8e8a68bbbc7b5
Deeraj <deerajnagothu10@gmail.com>
2016-04-14 18:21:51 -0400

    Added random coordinates generator

--- simulate_click.py
+++ simulate_click.py
@@ -1,30 +1,57 @@
 import pyautogui
+import random
 from selenium import webdriver
 print(pyautogui.size())
 width, height = pyautogui.size()
 pyautogui.FAILSAFE = False
 t = 75  # set a threshold value for origin points to click
 
+def generate_coordinates(width,height,coordinates):
+    difference=100
+    for i in range(75,width,difference):
+        for j in range(75,height,difference):
+            coordinates.append(i)
+            coordinates.append(j)
+    temp=[]
+    temp_coordinates=[]
+    for i in range(0,len(coordinates)):
+        if(i%2==0):
+            temp.append(coordinates[i])
+            temp.append(coordinates[i+1])
+            temp_coordinates.append(temp)
+            temp=[]
+    coordinates=temp_coordinates
+    return coordinates
 
-def clicker(flag):
+def generate_random_coordinates(coordinates):
+    temp_coordinates=[]
+    temp=[]
+    for i in range(0,len(coordinates)):
+        temp.append(i)
+    for i in range(0,len(coordinates)):
+        x=random.choice(temp)
+        temp_coordinates.append(coordinates[x])
+        temp.remove(x)
+    coordinates=temp_coordinates
+    return coordinates
+def clicker(flag,coordinates):
     if (flag == 1):
-        pyautogui.keyDown('ctrlleft')
-        for i in range(0, width, 50):
-            for j in range(0, height, 50):
-                print(t+j,t+i)
-                pyautogui.moveTo(t + j, t + i, duration=0.1)
-                pyautogui.click(t + j, t + i)
-        pyautogui.keyUp('ctrlleft')
+        for x in coordinates:
+            pyautogui.keyDown('ctrlleft')
+            print(x)
+            pyautogui.moveTo(x[0],x[1],duration=0.1)
+            pyautogui.click(x[0],x[1])
+            pyautogui.keyUp('ctrlleft')
 
-
-"""
-for i in range(5):
-      pyautogui.moveTo(100, 100, duration=1)
-      pyautogui.doubleClick(100,100)
-      pyautogui.moveTo(2560, 100, duration=1)
-      pyautogui.moveTo(2560, 1440, duration=1)
-      pyautogui.moveTo(100, 2560, duration=1)
-"""
-browser=webdriver.Chrome("C:\\Users\crawler\Desktop\Crawler\chromedriver.exe")
+coordinates=[]
+coordinates=generate_coordinates(width,height,coordinates)
+print (len(coordinates))
+print (coordinates)
+coordinates=generate_random_coordinates(coordinates)
+print("########################")
+print(len(coordinates))
+print(coordinates)
+browser=webdriver.Firefox()
 browser.get('https://www.flipkart.com')
-clicker(1)
+browser.maximize_window()
+clicker(1,coordinates)
